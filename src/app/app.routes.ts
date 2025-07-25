3bd0ab69cc5d5ff6db82c64307f3cfa43c588f3d
Rebecca Quinn <rebeccaq@codepaksolutions.com>
2025-07-25 13:46:04 +0100

    digitpet added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { GameComponent } from './game/game.component';
 import { HigherLowerComponent } from './game/higher-lower/higher-lower.component';
 import { LandingPageComponent } from './game/landing-page/landing-page.component';
 import { ChaserComponent } from './game/chaser/chaser.component';
+import { DigipetComponent } from './game/digipet/digipet.component';
 
 export const routes: Routes = [
     {
@@ -11,7 +12,8 @@ export const routes: Routes = [
         children: [
             { path: '', component: LandingPageComponent },
             { path: 'chaser', component: ChaserComponent },
-            { path: 'higher-lower', component: HigherLowerComponent }
+            { path: 'higher-lower', component: HigherLowerComponent },
+            { path: 'digipet', component: DigipetComponent }
         ]
     },
     { path: '', redirectTo: 'game', pathMatch: 'full' }
